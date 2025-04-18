1a7a86ff8bd0fe722e8dd093f4ab9942c18caa08
reiorozco <rfoc15@gmail.com>
2025-04-18 16:35:24 -0500

    edit not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,24 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle, ChevronLeft } from "lucide-react";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
+    <div className="container mx-auto max-w-3xl py-8">
+      <Alert>
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>Page Not Found</AlertTitle>
 
-      <Link href="/">Return Home</Link>
+        <AlertDescription>
+          Could not find requested resource
+          <Link href="/">
+            <Button className="mt-4" variant="outline">
+              <ChevronLeft className="h-4 w-4" /> Volver a proyectos
+            </Button>
+          </Link>
+        </AlertDescription>
+      </Alert>
     </div>
   );
 }
